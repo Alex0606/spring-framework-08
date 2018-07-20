ee932b741157c2b8c757afe723fbeea008c32be0
ydvorzhetskiy <dvorzhetskij@mail.ru>
2018-07-20 22:53:21 +0600

    -3

--- src/main/java/ru/otus/spring08/repostory/PersonRepositoryJpa.java
+++ src/main/java/ru/otus/spring08/repostory/PersonRepositoryJpa.java
@@ -17,11 +17,6 @@ public class PersonRepositoryJpa implements PersonRepository {
     private EntityManager em;
 
     @Override
-    public void insert(Person p) {
-        em.persist(p);
-    }
-
-    @Override
     public Person getById(int id) {
         return em.find(Person.class, id);
     }
